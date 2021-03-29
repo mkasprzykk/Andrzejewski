82ef6cfe3fa2c3c198dbac18155428a043091298
Marek Kasprzyk <marek.kasprzyk08@gmail.com>
2021-03-29 19:06:20 +0200

    zadanie 1 i 2

--- zadania_6.R
+++ zadania_6.R
@@ -4,3 +4,15 @@ tekst1 = c("TERYT 18; podkarpackie; Rzeszów; 0.2 He; A")
 tekst2 = c("TERYT 22; pomorskie;   Gdańsk; 12 C ; B")
 str_sub(tekst1, start = -5, end = -4)
 str_sub(tekst2, start = -5, end = -4)
+
+# zadanie 2
+
+horoskop = function(imie, miesiac) {
+  if (miesiac %% 2 == 0) {
+    cat("Osoba o imieniu", imie, "będzie miała jutro szczęście.")
+  } else {
+    cat("Osoba o imieniu", imie, "będzie miała jutro nieszczęście.")
+  }
+}
+
+horoskop("Marek", 11)
