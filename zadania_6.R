8deca7eab4b4b1ec996360f7e3ae1a55004282ed
Marek Kasprzyk <marek.kasprzyk08@gmail.com>
2021-03-31 19:48:28 +0200

    dodałem zadanie 3

--- zadania_6.R
+++ zadania_6.R
@@ -5,7 +5,7 @@ tekst2 = c("TERYT 22; pomorskie;   Gdańsk; 12 C ; B")
 str_sub(tekst1, start = -5, end = -4)
 str_sub(tekst2, start = -5, end = -4)
 
-# zadanie 2
+# ZADANIE 2
 
 horoskop = function(imie, miesiac) {
   if (miesiac %% 2 == 0) {
@@ -16,3 +16,21 @@ horoskop = function(imie, miesiac) {
 }
 
 horoskop("Marek", 11)
+
+# ZADANIE 3
+
+horoskop = function(imie, miesiac) {
+  if (str_detect(imie, pattern = "^K|^M|^Z") == TRUE) {
+    cat("Osoba o imieniu", imie, "będzie miała jutro szczęście.")
+  } else {
+    if (miesiac %% 2 == 0) {
+      cat("Osoba o imieniu", imie, "będzie miała jutro szczęście.")
+    } else {
+      cat("Osoba o imieniu", imie, "będzie miała jutro nieszczęście.")
+    }
+  }
+}
+horoskop("Marek", 9)
+
+# ZADANIE 4 
+tekst3 = c("2019-03-11: 23.5, 19/03/12: 12.7, 2019.03.13: 11.1, 2019-marzec-14: 14.3")
